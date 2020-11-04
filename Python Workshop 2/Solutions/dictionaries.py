2fa9201e9c833073616f1c8082f38853e80ba58b
Peter <34271471+petelampy@users.noreply.github.com>
2020-11-04 17:38:51 +0000

    Create dictionaries.py

---
+++ Python Workshop 2/Solutions/dictionaries.py
@@ -0,0 +1,18 @@
+infoDictionary = {
+    "forename": "Peter",
+    "surname": "Lampard",
+    "age": "19",
+    "hobby": "cars"
+}
+
+print(infoDictionary)
+
+
+print(infoDictionary["hobby"])
+
+
+infoDictionary.update({"DOB": "2001"})
+
+
+print(infoDictionary["DOB"])
+
