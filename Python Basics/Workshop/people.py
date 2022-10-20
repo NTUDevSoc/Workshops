a2f0d541407cce66b87300da6a3b48f8ddb4a839
Jake Mattless <78510383+JakeMattless@users.noreply.github.com>
2022-10-20 12:35:22 +0100

    Added source code to Python Basics workshop

---
+++ Python Basics/Workshop/people.py
@@ -0,0 +1,34 @@
+'''
+    An array of people, so that if we were to make a more complex version of guess who we could
+    randomly pick a character from this list if needed.
+    It would also make it easier to add attributes to each person individually if needed, since they're all located here.
+'''
+
+people = [
+    Person("Alex", "Black", "Brown", "Male"),
+    Person("Alfred", "Ginger", "Blue", "Male"),
+    Person("Anita", "Blonde", "Blue", "Female"),
+    Person("Anne", "Black", "Brown", "Female"),
+    Person("Bernard", "Brown", "Brown", "Male"),
+    Person("Bill", "Ginger", "Brown", "Male"),
+    Person("Charles", "Blonde", "Brown", "Male"),
+    Person("Claire", "Ginger", "Brown", "Female"),
+
+    Person("David", "Blonde", "Brown", "Male"),
+    Person("Eric", "Blonde", "Brown", "Male"),
+    Person("Frans", "Ginger", "Brown", "Male"),
+    Person("George", "White", "Brown", "Male"),
+    Person("Herman", "Ginger", "Brown", "Male"),
+    Person("Joe", "Blonde", "Brown", "Male"),
+    Person("Maria", "Brown", "Brown", "Female"),
+    Person("Max", "Black", "Brown", "Male"),
+
+    Person("Paul", "White", "Brown", "Male"),
+    Person("Peter", "White", "Blue", "Male"),
+    Person("Phillip", "Black", "Brown", "Male"),
+    Person("Richard", "Brown", "Brown", "Male"),
+    Person("Robert", "Brown", "Blue", "Male"),
+    Person("Sam", "White", "Brown", "Male"),
+    Person("Susan", "White", "Brown", "Female"),
+    Person("Tom", "Black", "Blue", "Male"),
+]
